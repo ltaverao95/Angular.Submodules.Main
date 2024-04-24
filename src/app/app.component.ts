3f2774c281c61c9478629149e2db168a3bf8c10c
natalia.tavera <natalia.tavera@flyrlabs.com>
2024-04-24 08:17:50 -0500

    Component footer added into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderWebComponent } from './modules/Header/header-container/header-web/header-web.component';
 import { BodyWebComponent } from './modules/Body/body-container/header-web/body-web.component';
+import { FooterWebComponent } from './modules/Footer/footer-container/footer-web/footer-web.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderWebComponent, BodyWebComponent],
+  imports: [RouterOutlet, HeaderWebComponent, BodyWebComponent, FooterWebComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
